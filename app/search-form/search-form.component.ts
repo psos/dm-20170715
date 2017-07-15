86e3fa0f36543968418ef0f6d89568b39b70cd33
Pawel Sosnicki <sosnicki.pawel@gmail.com>
2017-07-15 14:09:04 +0200

    Task 3 / step 1 / Events

---
+++ app/search-form/search-form.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { FormControl } from '@angular/forms';
+
+@Component({
+  selector: 'app-search-form',
+  templateUrl: './search-form.component.html',
+  styleUrls: ['./search-form.component.css']
+})
+export class SearchFormComponent implements OnInit {
+  public searchInput = new FormControl();
+  @Output() public changeSearch = new EventEmitter();
+
+  ngOnInit() {
+  }
+
+  constructor() {
+    this.searchInput.valueChanges.subscribe((e) => {
+          this.changeSearch.next(e);
+        }  
+    );
+  }
+
+}
