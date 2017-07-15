c8be4d5cb1d3aac0b75b40d7cea7c10cac3f87f2
Paweł Sośnicki <Pawe Sonicki>
2017-07-15 10:39:50 +0200

    Task1

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,10 +1,43 @@
 import { Component } from '@angular/core';
 
+// interfejs Produktu
+interface Product {
+  id: number;
+  name : string;
+  price : number;
+  currency: string;
+  promoted : boolean;
+}
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
   title = 'app';
+  // store lista produktów
+  public products:Product[] = [
+    { 
+        id : 1,
+        name : 'Kawa',
+        price : 5.99,
+        currency : 'PLN',
+        promoted : true
+    },
+    { 
+        id : 2,
+        name : 'Hot dog',
+        price : 3.99,
+        currency : 'PLN',
+        promoted : false,
+    },
+    { 
+        id : 3,
+        name : 'Piwo żywiec',
+        price : 2.99,
+        currency : 'PLN',
+        promoted : false,
+    },      
+  ];
 }
